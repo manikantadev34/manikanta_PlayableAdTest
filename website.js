f9a1ee4f8e4591a3590dfac9d0de8253bf39f7fd
manikantadev34 <manikantakunchanapali@gmail.com>
2025-03-27 18:15:21 +0530

    Plzyable ads

--- website.js
+++ website.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // Show the website content initially
     setTimeout(() => {
-        // Hide the website content
         document.getElementById("websiteContent").style.display = "none";
-        // Show the game page
         document.getElementById("gamePage").style.display = "block";
 
-        // Redirect to Google Play Store after 15 seconds
+        // After 15 seconds, redirect to the Play Store
         setTimeout(() => {
             window.location.href = "https://play.google.com/store/apps/details?id=com.arcsys.tictactoe&hl=en_IN";
         }, 15000);
-    }, 10000); // Switch from website to game after 10 seconds
+    }, 10000); // Show website for 10 seconds
 });
